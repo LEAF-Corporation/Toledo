fef26fff7fe9f2135634c2903e890fc4d09c44fb
kmuv1t <carlosborgesbenjamin@gmail.com>
2023-08-15 19:28:17 -0300

    BEV (Bird's Eye View) Car 360 Degree Cam

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Toledo
-Challenge Sprint 2023 - Toledo
+## Challenge Sprint 2023 - Toledo
 
-Componentes do Grupo:
+---
 
-Carlos Borges - 4ECR RM84613
-Erik Evaristo - 4ECR RM81052
-Eduardo Stefano - 4ECR RM84498
-Fabio Pipek - 4ECA RM84566
-Guilherme Cabrini - 4ECA RM84189
-Marcos Cantelli - 4ECA RM84582
+**Componentes do Grupo:**
+
+> 4ECR
+- Carlos Borges - RM84613
+- Erik Evaristo - RM81052
+- Eduardo Stefano - RM84498
+
+> 4ECA
+- Fabio Pipek - 4ECA RM84566
+- Guilherme Cabrini - 4ECA RM84189
+- Marcos Cantelli - 4ECA RM84582
--- main.py
+++ main.py
@@ -1,13 +1,53 @@
-from ultralytics import YOLO
-import torch
+import cv2
+import numpy as np
+import cv2 as cv
 
-model = YOLO("models/yolov8s-seg.pt")
-
-
-def model_training():
-    model.predict(source="0", show=True)
+cap = cv.VideoCapture(0)
 
 
-if __name__ == '__main__':
-    if torch.__version__.split('+')[1].startswith('cu') and torch.cuda.is_available():
-        model_training()
+def captura():
+    if not cap.isOpened():
+        print("Cannot open camera")
+        return
+    while True:
+        ret, frame = cap.read()
+        frame = cv.resize(frame, (640, 480))
+
+        bl = (640, 480)
+        br = (640, 0)
+        tl = (480, 300)
+        tr = (480, 150)
+
+        cv.circle(frame, tl, 5, (0, 255, 0), -1)
+        cv.circle(frame, bl, 5, (0, 255, 0), -1)
+        cv.circle(frame, tr, 5, (0, 255, 0), -1)
+        cv.circle(frame, br, 5, (0, 255, 0), -1)
+
+        pts1 = np.float32([tl, bl, tr, br])
+        pts2 = np.float32([[0, 0], [0, 480], [640, 0], [640, 480]])
+
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        transformed = cv2.warpPerspective(frame, matrix, (640, 480))
+
+        cv.imshow('Frame', frame)
+        cv.imshow('BEV Frame', transformed)
+
+        if cv.waitKey(1) == ord('q'):
+            break
+
+captura()
+"""
+while True:
+    # Capture frame-by-frame
+    
+    # if frame is read correctly ret is True
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', gray)
+    # Display the resulting frame
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+"""
