cb7fd8149ac056875a44bba83cd7d23b07af33ad
kmuv1t <carlosborgesbenjamin@gmail.com>
2023-09-14 04:37:00 -0300

    Código de mapeamento do Lidar corrigido.

--- lidar_esp32.py
+++ lidar_esp32.py
@@ -1,67 +1,49 @@
+import pathlib
 import socket
-import csv
-import datetime
-import plotly.express as px
-import plotly.graph_objects as go
+import traceback
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
-import os
-import time
-
-
-def map(x, inmin, inmax, outmin, outmax):
-    return int((x - inmin) * (outmax - outmin) / (inmax - inmin) + outmin)
-
+import plotly.graph_objects as go
+import plotly.express as px
 
 while True:
-    c = input("Realizar leitura de arquivos ou scan? [f|s]\nPara sair digite 'q': ")
     df = pd.DataFrame(columns=['x', 'y', 'z', 's'])
 
-    if c == 'f':
-        paths = []
-        selPath = ""
-        c = 0
-        for root, dirs, files in os.walk("."):
-            for file in files:
-                if file.endswith(".csv"):
-                    print(str(c) + ": " + os.path.join(root, file))
-                    paths.append(os.path.join(root, file))
-                    c += 1
-        c = int(input("Escolha o arquivo: "))
+    sel = input("Realizar leitura de arquivos ou scan? [f|s]\nPara sair digite 'q': ")
+    if sel.lower() == 'f':
+        paths=[]
+        search = pathlib.Path(pathlib.Path().resolve())
+        for item in search.iterdir():
+            if item.is_file():
+                if item.name.split('.')[1] == 'csv':
+                    paths.append(item)
 
-        while c >= len(paths):
-            c = int(input(f"Existem apenas {len(paths)} arquivo(s). Escolha um deles: "))
-        selPath = str(paths[c]).replace('.\\', '')
-        print(f"Carregando {selPath}...")
+        if len(paths) <= 0:
+            print('Nenhum arquivo encontrado...')
+            continue
 
-        rows=0
-        with open(selPath) as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=',')
-            rows = int(len(list(readCSV)))
-            print("Linhas: " + str(rows))
+        print([f'{paths.index(x)} - {x}' for x in paths])
+        fname = int(input("Escolha o arquivo: "))
+        while fname >= len(paths):
+            fname = int(input(f"Existem apenas {len(paths)} arquivo(s). Escolha um deles: "))
+        selPath = paths[fname]
+        csv_df = pd.read_csv(selPath, delimiter=',')
+        print(f"Carregando {selPath}... ")
 
-        barSize = 50
-        with open(selPath) as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=',')
-            next(readCSV)
+        for i, rows in csv_df.iterrows():
+            dist = float(rows[2])
+            strength = float(rows[3])
+            x = float(rows[4])
+            y = float(rows[5])
+            z = float(rows[6])
+            if dist <= 1200 or dist <= 0:
+                if strength <= 20000.0:
+                    dftemp = {'x': x, 'y': y, 'z': z, 's': strength}
+                    df.loc[len(df)] = dftemp
 
-            c=0
-            for row in readCSV:
-                dist = float(row[2])
-                strength = float(row[3])
-                x = float(row[4])
-                y = float(row[5])
-                z = float(row[6])
-                if dist <= 1200 or dist <= 0:
-                    if strength <= 20000.0:
-                        dftemp = {'x': x, 'y': y, 'z': z, 's': strength}
-                        df = df.append(dftemp, ignore_index=True)
-                        print(
-                            str(float(row[0])) + "," + str(float(row[1])) + "," + str(dist) + "," + str(strength) + "|")
-
-        print("\n")
-
-    elif c == 's':
+    elif sel.lower() == 's':
         host = socket.gethostbyname(socket.gethostname())
         port = 8090
 
@@ -69,72 +51,69 @@ while True:
         s.bind((host, port))
         s.listen(0)
 
-        # get time and date
-        timestamp = datetime.datetime.now()
-
-        # create CSV
-        path = str(timestamp.day) + "-" + str(timestamp.month) + "-" + str(timestamp.year) + "_" + str(
-            timestamp.hour) + "-" + str(timestamp.minute) + "-" + str(timestamp.second) + ".csv"
-        print(path)
-
         print("Buscando pelo Client")
-        (client, addr) = s.accept()
-        print("\rClient conectado!")
+        client, addr = s.accept()
+        print("Client conectado!")
 
-        file = open(path, 'w', encoding='UTF8', newline='')
-        writer = csv.writer(file)
-        writer.writerow(["horizontal", "vertical", "depth", "strength", "x", "y", "z"])
+        scan_df = pd.DataFrame(columns=["horizontal", "vertical", "depth", "strength", "x", "y", "z"])
+        print("Escaneando...")
 
         longSet = []
-        final = []
-
-        print("Escaneando...")
         while True:
-            raw = client.recv(64)
-            longSet.append(raw.decode('utf-8'))
+            raw = client.recv(1024)
+            try:
+                data = raw.decode('utf-8').split('|')
+                for x in data:
+                    if len(x) > 0:
+                        print(x)
+                        longSet.append(x)
 
-            if len(raw) == 0:
-                print("Pronto!")
-                break
+                if len(raw) == 0:
+                    print("Scan finalizado!")
+                    break
+
+            except Exception as err:
+                print(err)
+                traceback.print_exc()
 
         client.close()
         s.close()
+
         print("Calculando resultado...")
-        final = str("".join(longSet)).split("|")
-        for e in final:
+        for e in longSet:
             try:
                 bites = str(e).split(",")
                 x = float(bites[2]) * np.cos(np.deg2rad(float(bites[1]))) * np.cos(np.deg2rad(float(bites[0])))
                 y = float(bites[2]) * np.sin(np.deg2rad(float(bites[1]))) * np.cos(np.deg2rad(float(bites[0])))
                 z = float(bites[2]) * np.sin(np.deg2rad(float(bites[0])))
 
-                writer.writerow([bites[0], bites[1], bites[2], bites[3], x, y, z])
+                scan_df.loc[len(scan_df)] = [bites[0], bites[1], bites[2], bites[3], x, y, z]
+
                 if float(bites[2]) <= 1200 or float(bites[2]) == 0:
                     if float(bites[3]) <= 20000.0:
                         dftemp = {'x': x, 'y': y, 'z': z, 's': float(bites[3])}
-                        df = df.append(dftemp, ignore_index=True)
-
+                        df.loc[len(df)] = dftemp
                         if len(df) % 100 == 0:
-                            # loading(c)
-                            if c == 4:
-                                c = 0
-                            else:
-                                c += 1
+                            continue
             except Exception as e:
                 print(f"Erro, {e}")
+                traceback.print_exc()
+
+        # Salvando CSV
+        path = f"Lidar {datetime.strftime(datetime.now(), '%d_%m_%Y %H_%M_%S')}.csv"
+        print(f'Criando arquivo csv: {path}')
+        scan_df.to_csv(path, index=False)
 
         print("Fim.")
-
-        file.close()
-    elif c == 'q':
+    elif sel == 'q':
         exit(0)
     else:
-        print("Input inválido. Encerrando...")
-        exit(0)
+        print('Input inválido, tente novamente.')
+        continue
 
     print("Abrindo gráfico de dispersão...")
 
-    ##3d Plot
+    # Plotagem 3D
     x = df['x']
     y = df['y']
     z = df['z']
